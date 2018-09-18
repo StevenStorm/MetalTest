f1e4380479644486b8b18e03b4c977d5ed248de8
István Gulyás <istvan_gulyas@epam.com>
2018-09-18 12:45:00 +0200

    Initial Commit

---
+++ MetalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalTest
+//
+//  Created by Local István Gulyás on 9/18/18.
+//  Copyright © 2018 Local István Gulyás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
